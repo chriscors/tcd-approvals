c21aa181af1e2ccbfdf6e8a41d3dc49b6a07d6a6
chriscors <70537376+chriscors@users.noreply.github.com>
2024-10-20 21:56:11 -0400

    Add footer

--- src/app/(main)/approve/[timecard_id]/table.tsx
+++ src/app/(main)/approve/[timecard_id]/table.tsx
@@ -6,7 +6,7 @@ import {
   MRT_ColumnDef,
   useMantineReactTable,
 } from "mantine-react-table";
-import React from "react";
+import React, { useMemo } from "react";
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import { Badge, Box, Text } from "@mantine/core";
@@ -14,97 +14,131 @@ import { Badge, Box, Text } from "@mantine/core";
 dayjs.extend(customParseFormat);
 
 type TData = Ttimecardline;
+export default function MyTable({ data }: { data: TData[] }) {
+  const totalPay = useMemo(() => {
+    return data.reduce((acc, row) => {
+      return acc + Number(row.dollarsTotalPay_c);
+    }, 0);
+  }, [data]);
 
-const columns: MRT_ColumnDef<TData>[] = [
-  {
-    header: "Hours",
-    id: "time",
-    Cell: ({ row }) => {
-      const timeIn = dayjs(row.original.time_in, "HH:mm:ss").format("h:mm A");
-      const timeOut = dayjs(row.original.time_out, "HH:mm:ss").format("h:mm A");
-      return timeIn === "Invalid Date" ? (
-        ""
-      ) : (
-        <Box>
+  const columns: MRT_ColumnDef<TData>[] = [
+    // {
+    //   header: "Hours",
+    //   id: "time",
+    //   Cell: ({ row }) => {
+    //     const timeIn = dayjs(row.original.time_in, "HH:mm:ss").format("h:mm A");
+    //     const timeOut = dayjs(row.original.time_out, "HH:mm:ss").format("h:mm A");
+    //     return timeIn === "Invalid Date" ? (
+    //       ""
+    //     ) : (
+    //       <Box>
+    //         <Box>
+    //           <Text size="sm" fw={500} c="dimmed">
+    //             Time In
+    //           </Text>
+    //           <Text>{`${timeIn} - ${timeOut}`}</Text>
+    //         </Box>
+
+    //       </Box>
+    //     );
+    //   },
+    // },
+    {
+      id: "job",
+
+      header: "Job",
+      Cell: ({ row }) => {
+        const timeIn = dayjs(row.original.time_in, "HH:mm:ss").format("h:mm A");
+        const timeOut = dayjs(row.original.time_out, "HH:mm:ss").format(
+          "h:mm A"
+        );
+        const Time =
+          timeIn === "Invalid Date" ? (
+            ""
+          ) : (
+            <Box>
+              <Box>
+                <Text size="sm" fw={500} c="dimmed">
+                  Time In
+                </Text>
+                <Text>{`${timeIn} - ${timeOut}`}</Text>
+              </Box>
+            </Box>
+          );
+        return (
           <Box>
-            <Text size="sm" fw={500} c="dimmed">
-              Time In
-            </Text>
-            <Text>{`${timeIn} - ${timeOut}`}</Text>
-          </Box>
-          {row.original.hrsWorked_num_c && (
+            {Time}
             <Box>
               <Text size="sm" fw={500} c="dimmed">
-                Worked Hours
+                Call
               </Text>
-              <Text>{`${row.original.hrsWorked_num_c || 0}`}</Text>
+              <Text>
+                {row.original["TCL_EVS__EventSchedule::vl_display_evs_c"]}
+              </Text>
             </Box>
-          )}
-
-          {row.original.hrsUnworked_num_c && (
-            <Box display={"inline"}>
+            <Box>
               <Text size="sm" fw={500} c="dimmed">
-                Unworked Hours
+                Position
               </Text>
-              <Text>{`${row.original.hrsUnworked_num_c}`}</Text>
+              <Text>{row.original["TCL_CJT__ContractJobTitle::Name"]}</Text>
+              <Badge variant="light" ml={"sm"}>
+                {row.original["TCL_RTC__RateCard::name"]}
+              </Badge>
             </Box>
-          )}
-        </Box>
-      );
+          </Box>
+        );
+      },
     },
-  },
-  {
-    id: "job",
-    header: "Job",
-    Cell: ({ row }) => {
-      return (
-        <Box>
+    {
+      id: "pay",
+      header: "Pay",
+      Cell: ({ row }) => {
+        return (
           <Box>
-            <Text size="sm" fw={500} c="dimmed">
-              Call
-            </Text>
-            <Text>
-              {row.original["TCL_EVS__EventSchedule::vl_display_evs_c"]}
-            </Text>
-          </Box>
-          <Box>
-            <Text size="sm" fw={500} c="dimmed">
-              Position
-            </Text>
-            <Text>{row.original["TCL_CJT__ContractJobTitle::Name"]}</Text>
-            <Badge variant="light" ml={"sm"}>
-              {row.original["TCL_RTC__RateCard::name"]}
-            </Badge>
-          </Box>
-        </Box>
-      );
-    },
-  },
-  {
-    id: "pay",
-    header: "Pay",
-    Cell: ({ row }) => {
-      return (
-        <Box>
-          <Box>
-            <Text size="sm" fw={500} c="dimmed">
-              Rate
-            </Text>
-            <Text>{row.original.rateFinal_c}</Text>
-          </Box>
-          <Box>
-            <Text size="sm" fw={500} c="dimmed">
-              Pay
-            </Text>
-            <Text>{row.original.dollarsTotalPay_c}</Text>
-          </Box>
-        </Box>
-      );
-    },
-  },
-];
+            {row.original.hrsWorked_num_c && (
+              <Box>
+                <Text size="sm" fw={500} c="dimmed">
+                  Worked Hours
+                </Text>
+                <Text>{`${row.original.hrsWorked_num_c || 0}`}</Text>
+              </Box>
+            )}
 
-export default function MyTable({ data }: { data: TData[] }) {
+            {row.original.hrsUnworked_num_c && (
+              <Box display={"inline"}>
+                <Text size="sm" fw={500} c="dimmed">
+                  Unworked Hours
+                </Text>
+                <Text>{`${row.original.hrsUnworked_num_c}`}</Text>
+              </Box>
+            )}
+            <Box>
+              <Text size="sm" fw={500} c="dimmed">
+                Rate
+              </Text>
+              <Text>${Number(row.original.rateFinal_c).toFixed(2)}</Text>
+            </Box>
+            <Box>
+              <Text size="sm" fw={500} c="dimmed">
+                Pay
+              </Text>
+              <Text>${Number(row.original.dollarsTotalPay_c).toFixed(2)}</Text>
+            </Box>
+          </Box>
+        );
+      },
+      Footer: () => (
+        <Box>
+          <Text size="sm" fw={500} c="dimmed">
+            Total Pay
+          </Text>
+          <Text>${totalPay.toFixed(2)}</Text>
+        </Box>
+      ),
+    },
+  ];
+  console.log(data);
+
   const table = useMantineReactTable({
     data,
     columns,
@@ -116,6 +150,7 @@ export default function MyTable({ data }: { data: TData[] }) {
       shadow: "none",
       withBorder: true,
     },
+    enableStickyFooter: true,
   });
   return <MantineReactTable table={table} />;
 }
