27c3dfdf345e61bb0e2d816e002c959a1d40d456
chriscors <70537376+chriscors@users.noreply.github.com>
2024-10-20 20:05:04 -0400

    remove nav, home

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -1,17 +1,17 @@
 import { type ProofKitRoute } from "@proofgeist/kit";
 
 export const primaryRoutes: ProofKitRoute[] = [
-  {
-    label: "Dashboard",
-    type: "link",
-    href: "/",
-    exactMatch: true,
-  },
-  {
-    label: "approve",
-    type: "link",
-    href: "/approve",
-  },
+  // {
+  //   label: "Dashboard",
+  //   type: "link",
+  //   href: "/",
+  //   exactMatch: true,
+  // },
+  // {
+  //   label: "approve",
+  //   type: "link",
+  //   href: "/approve",
+  // },
 ];
 
 export const secondaryRoutes: ProofKitRoute[] = [];
